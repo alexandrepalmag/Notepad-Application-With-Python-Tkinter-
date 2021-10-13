f48df84f9d01665f85f94ca9aa070a159c107506
alexandrepalmag <alepalmag85ti@gmail.com>
2021-10-13 01:04:11 -0300

    Creation of the application window set size properties.

---
+++ NotepadApplication.py
@@ -0,0 +1,13 @@
+import tkinter
+
+#Creation of the application window.
+window = tkinter.Tk()
+#window properties
+window.title("NotepadApp")
+window.geometry("1280x720")#or window.minsize(width=1280, height=720)
+
+window.mainloop()
+
+
+
+
